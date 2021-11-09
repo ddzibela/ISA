ece356da8bf24055db5509a0f742aa650f216af2
Denis <d.dzibela0000@gmail.com>
2021-11-09 23:30:08 +0100

    split optArgs struct into its own header

---
+++ optArgs.hpp
@@ -0,0 +1,13 @@
+#pragma once
+struct optArgs {
+	bool r, s, l; //option flags
+	bool u; //used for unknown option
+	std::string r_specifier;
+	std::string s_specifier;
+	optArgs() :
+		r(false),
+		s(false),
+		l(false),
+		u(false)
+	{}
+};
