1f8b84101423937f5a35b7b5595ffa502d54308e
Denis <22411163+ddzibela@users.noreply.github.com>
2021-11-10 17:03:31 +0100

    get file name from path, start work on sending packets

--- client.cpp
+++ client.cpp
@@ -13,22 +13,45 @@ client::client(optArgs* args)
 	}
 }
 
-void client::sendFile()
+void client::getFileName(const std::string filePath)
 {
-	addrinfo* serverInfo;
-	getAddr(this->hostName.c_str(), serverInfo);
+	std::size_t found = filePath.find_last_of("/\\");
+	this->fileName = filePath.substr(found + 1);
 }
-
 
 void client::getAddr(const char* host, addrinfo* serverInfo)
 {
 	addrinfo hints;
 	int result;
+
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_RAW;
+	
 	if ((result = getaddrinfo(host, NULL, &hints, &serverInfo)) != 0)
 	{
 		//std::cerr << gai_strerror(result);
 		throw std::runtime_error(gai_strerror(result));
 	}
+}
+
+void client::sendFile()
+{
+	addrinfo* serverInfo;
+	getAddr(this->hostName.c_str(), serverInfo);
+	this->icmpv = ((serverInfo->ai_family == AF_INET) ? IPPROTO_ICMP : IPPROTO_ICMPV6);//determine protocol version from ip version
+
+	//open raw socket
+	int sock = socket(serverInfo->ai_family, serverInfo->ai_socktype, this->icmpv);
+	if (sock == -1) {
+		throw std::runtime_error("Error opening socket.");
+	}
+
+	//icmp header construction
+	//file operations
+	//encryption
+	//icmp checksum calculation
+	//send packet
+	//listen for return packet 
+	//poll to check we can write again
+	//repetitio ad absurdum
 }
--- client.hpp
+++ client.hpp
@@ -4,15 +4,19 @@
 #include <netdb.h>	   // getaddrinfo
 #include <string.h>	   //memset
 #include <arpa/inet.h> // inet_ntop
-#include <netinet/ip_icmp.h>
-#include <openssl/aes.h>
+#include <netinet/ip_icmp.h> //icmp headers
+#include <openssl/aes.h> //AES symmetric encryption
 
 class client
 {
 private:
 	std::string hostName;
 	std::string filePath;
+	std::string fileName;
+	int icmpv; //ICMP version
+
 	void getAddr(const char* host, struct addrinfo* serverInfo);
+	void getFileName(const std::string filePath);
 public:
 	void sendFile();
 	client(optArgs* args);
