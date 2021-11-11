4eccbfe2b16c33e99a5daf56cbf7783627d2991d
Denis <22411163+ddzibela@users.noreply.github.com>
2021-11-11 19:29:20 +0100

    add error handling

--- main.cpp
+++ main.cpp
@@ -67,11 +67,13 @@ int main(int argc, char** argv)
 			client cli(&options);
 			cli.sendFile();
 		}
-		catch (std::invalid_argument e) {
-
+		catch (std::invalid_argument& e) {
+			std::cerr << e.what() << std::endl;
+			return 1;
 		}
-		catch (std::runtime_error e) {
-
+		catch (std::runtime_error& e) {
+			std::cerr << e.what() << std::endl;
+			return 1;
 		}
 		return 0;
 	}
