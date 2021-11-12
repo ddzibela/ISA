a5f50458992d1538f2382b17335d78cd6412cd1c
Denis <22411163+ddzibela@users.noreply.github.com>
2021-11-12 01:29:13 +0100

    make custom protocol headers

---
+++ icmp.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstdint>
+#include <fstream>
+
+struct icmpEcho
+{
+	uint8_t type;
+	uint8_t code;
+	uint16_t checksum;
+	uint16_t identifier;
+	uint16_t sequence;
+};
+
+struct secretProtoEstablish
+{
+	uint16_t magic;
+	uint64_t nameLen;
+	uint64_t size;
+	char* name;
+};
+
+struct secretProtoTransfer
+{
+	uint16_t magic;
+	char* data;
+};
